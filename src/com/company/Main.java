fa88c69eff439a70ec1c5c8e1f0cc1d3c52187d2
Dmitry <dmitry.osichkin@gmail.com>
2018-09-04 19:38:49 +0300

    vcs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,27 +10,53 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println(myNum);
-        boolean userLost = true;
-        for (int i = 1 ; i < 10; i += 1) {
-            System.out.println("Try #" + i);
-            int userNum = scan.nextInt();
-            if (myNum > userNum) {
-                System.out.println("a littel bit more");
+        String answer;
+        int scanNum = 0;
+        do {
+
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println(myNum);
+            boolean userLost = true;
+            for (int i = 1; i < 10; i += 1) {
+                System.out.println("Try #" + i);
+                int userNum = scan.nextInt();
+                if (myNum > userNum) {
+                    System.out.println("a littel bit more");
+                }
+                if (myNum < userNum) {
+                    System.out.println("slightly less");
+
+                }
+                if (myNum == userNum) {
+                    System.out.println("good");
+                    userLost = false;
+                    break;
+                }
             }
-            if (myNum < userNum) {
-                System.out.println("slightly less");
+            if (userLost) {
+                System.out.println("You lost");
 
             }
-            if (myNum == userNum) {
-                System.out.println("good");
-                userLost = false;
+            System.out.println("Do you want play again? (y/n)");
+            answer = askYN();
+
+
+        } while (answer.equals("y"));
+        System.out.println("good bye!");
+    }
+
+    static String askYN() {
+        String answer;
+        do {
+            answer = scan.next();
+            if (!answer.equals("y")&& !answer.equals("n")) {
+                System.out.println("you can enter only 'y' or 'n' ");
+                continue;
+            } else {
                 break;
-
             }
-        }
-        if (userLost == true)
-        System.out.println("You lost");
+        } while (true);
+        return answer;
     }
 }
+
